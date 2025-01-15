c28368104d308b6d833e8e3729ca36729b137df5
cho <aaaapple123@naver.com>
2025-01-15 17:18:27 +0900

    Initial commit

---
+++ model/yolo-v1/README.md
@@ -0,0 +1,4 @@
+# One Stage vs Two Stage
+
+- One Stage : 한번에 예측하는 방식
+- Two Stage : 두번에 예측하는 방식
---
+++ model/yolo-v1/model.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+""" 
+Information about architecture config:
+Tuple is structured by (kernel_size, filters, stride, padding) 
+"M" is simply maxpooling with stride 2x2 and kernel 2x2
+List is structured by tuples and lastly int with number of repeats
+"""
+
+architecture_config = [
+    (7, 64, 2, 3),
+    "M",
+    (3, 192, 1, 1),
+    "M",
+    (1, 128, 1, 0),
+    (3, 256, 1, 1),
+    (1, 256, 1, 0),
+    (3, 512, 1, 1),
+    "M",
+    [(1, 256, 1, 0), (3, 512, 1, 1), 4],
+    (1, 512, 1, 0),
+    (3, 1024, 1, 1),
+    "M",
+    [(1, 512, 1, 0), (3, 1024, 1, 1), 2],
+    (3, 1024, 1, 1),
+    (3, 1024, 2, 1),
+    (3, 1024, 1, 1),
+    (3, 1024, 1, 1),
+]
